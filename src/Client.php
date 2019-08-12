fa41a07b5b8bf729e11d044f999e43883ec395b4
Harry Doddema <harry@niomail.nl>
2019-08-12 13:27:26 +0200

    + Document entity type

--- src/Client.php
+++ src/Client.php
@@ -3,95 +3,101 @@
 namespace nymedia\SuperOffice;
 
 use nymedia\SuperOffice\resources\Contact;
+use nymedia\SuperOffice\resources\Document;
 use nymedia\SuperOffice\resources\Person;
 use nymedia\SuperOffice\resources\Project;
 use nymedia\SuperOffice\resources\ProjectMember;
 
-class Client {
+class Client
+{
 
-  /**
-   * @var null|\GuzzleHttp\Client
-   */
-  protected $client;
+    /**
+     * @var null|\GuzzleHttp\Client
+     */
+    protected $client;
 
-  protected $url;
+    protected $url;
 
-  protected $password;
+    protected $password;
 
-  protected $user;
+    protected $user;
 
-  protected $language = 'en';
+    protected $language = 'en';
 
-  public function getClient()
-  {
-    if (!$this->client) {
-      $this->client = new \GuzzleHttp\Client();
-    }
-    return $this->client;
-  }
-
-  public function __construct($url, $user, $password, $client = null, $language)
-  {
-    $this->url = $url;
-    $this->client = $client;
-    $this->user = $user;
-    $this->password = $password;
-    $this->language = $language;
-  }
-
-  public function projectMember()
-  {
-    return new ProjectMember($this);
-  }
-
-  public function person()
-  {
-    return new Person($this);
-  }
-
-  public function contact()
-  {
-    return new Contact($this);
-  }
-
-  public function project()
-  {
-    return new Project($this);
-  }
-
-  public function get($path, $data = null)
-  {
-    return $this->apiCall('GET', $this->url . '/' . $path, $data);
-  }
-
-  public function post($path, $data)
-  {
-    return $this->apiCall('POST', $this->url . '/' . $path, $data);
-  }
-
-  public function put($path, $data)
-  {
-    return $this->apiCall('PUT', $this->url . '/' . $path, $data);
-  }
-
-  protected function apiCall($method, $path, $data = null)
-  {
-    $opts = [
-      'headers' => [
-        'User-Agent' => 'Superoffice PHP SDK (https://github.com/nymedia/superoffice-php-sdk)',
-        'Accept' => 'application/json',
-        'Accept-Language' => $this->language,
-      ],
-      'auth' => [$this->user, $this->password],
-    ];
-    if ($data && $method != 'GET') {
-      // Set all needed options with this shorthand.
-      $opts['json'] = $data;
-    }
-    elseif ($data) {
-      $opts['query'] = $data;
+    public function getClient()
+    {
+        if (!$this->client) {
+            $this->client = new \GuzzleHttp\Client();
+        }
+        return $this->client;
     }
 
-    return $this->getClient()->request($method, $path, $opts);
-  }
+    public function __construct($url, $user, $password, $client = null, $language)
+    {
+        $this->url = $url;
+        $this->client = $client;
+        $this->user = $user;
+        $this->password = $password;
+        $this->language = $language;
+    }
+
+    public function projectMember()
+    {
+        return new ProjectMember($this);
+    }
+
+    public function person()
+    {
+        return new Person($this);
+    }
+
+    public function contact()
+    {
+        return new Contact($this);
+    }
+
+    public function project()
+    {
+        return new Project($this);
+    }
+
+    public function document()
+    {
+        return new Document($this);
+    }
+
+    public function get($path, $data = null)
+    {
+        return $this->apiCall('GET', $this->url . '/' . $path, $data);
+    }
+
+    public function post($path, $data)
+    {
+        return $this->apiCall('POST', $this->url . '/' . $path, $data);
+    }
+
+    public function put($path, $data)
+    {
+        return $this->apiCall('PUT', $this->url . '/' . $path, $data);
+    }
+
+    protected function apiCall($method, $path, $data = null)
+    {
+        $opts = [
+            'headers' => [
+                'User-Agent' => 'Superoffice PHP SDK (https://github.com/nymedia/superoffice-php-sdk)',
+                'Accept' => 'application/json',
+                'Accept-Language' => $this->language,
+            ],
+            'auth' => [$this->user, $this->password],
+        ];
+        if ($data && $method != 'GET') {
+            // Set all needed options with this shorthand.
+            $opts['json'] = $data;
+        } elseif ($data) {
+            $opts['query'] = $data;
+        }
+
+        return $this->getClient()->request($method, $path, $opts);
+    }
 }
---
+++ src/resources/Document.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace nymedia\SuperOffice\resources;
+
+use nymedia\SuperOffice\RestBase;
+
+class Document extends RestBase
+{
+
+  protected $resourcePath = 'Document';
+
+}
